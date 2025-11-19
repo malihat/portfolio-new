951b2df1a592014839459a5de17844396b9e56ca
Maliha Tanweer <malihatanweer@gmail.com>
2025-11-19 11:26:27 -0500

    changed icons url

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,24 +1,29 @@
+import Code from '../assets/Code.png';
+import Framework from '../assets/Framework.png';
+import Version from '../assets/Version.png';
+import Design from '../assets/Design.png';
+
 const Skills = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-10">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 text-center">
         <div className="border border-gray-500 radius-md rounded-xl shadow p-4 flex flex-col items-center">
-          <img src='./public/Code.png' id="skill-icon" className="object-contain mb-2"/>
+          <img src={Code} id="skill-icon" className="object-contain mb-2"/>
           <h2 className="font-semibold text-lg text-white font-md mb-2 mt-3">Languages</h2>
           <p className="text-white text-base text-sm">HTML5, CSS3, JavaScript (ES5, ES6)</p>
         </div>
         <div className="border border-gray-500 radius-md rounded-xl shadow p-4 flex flex-col items-center">
-          <img src='./public/Framework.png' id="skill-icon" className="object-contain mb-2" />
+          <img src={Framework} id="skill-icon" className="object-contain mb-2" />
           <h2 className="font-semibold text-lg text-white font-md mb-2 mt-3">Frameworks</h2>
           <p className="text-white text-base text-sm">Bootstrap, jQuery, React, Redux, Express, Mongoose</p>
         </div>
         <div className="border border-gray-500 radius-md rounded-xl shadow p-4 flex flex-col items-center">
-          <img src='./public/Version.png' id="skill-icon" className="object-contain mb-2" />
+          <img src={Version} id="skill-icon" className="object-contain mb-2" />
           <h2 className="font-semibold text-lg text-white font-md mb-2 mt-3">Version Control</h2>
           <p className="text-white text-base text-sm">Git and Github</p>
         </div>
         <div className="border border-gray-500 radius-md rounded-xl shadow p-4 flex flex-col items-center">
-          <img src='./public/Design.png' id="skill-icon" className="object-contain mb-2" />
+          <img src={Design} id="skill-icon" className="object-contain mb-2" />
           <h2 className="font-semibold text-lg text-white font-md mb-2 mt-3">Design</h2>
           <p className="text-white text-base text-sm">Figma, Photopea</p>
         </div>
